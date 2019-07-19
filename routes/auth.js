c25f79945cd26680736e0e606d9c4bb4b979b1e2
Xiaoyu <yummyuuu@qq.com>
2019-07-19 16:41:05 +0800

    router.post

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,20 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  res.send("GET request to the homepage");
+  res.send("GET request to the auth");
   console.log("success");
 });
 /* POST */
+router.get("/login", function(req, res) {
+  res.json([
+    { username: "test", password: "test001" },
+    { username: "test", password: "test002" }
+  ]);
+  console.log("success-login");
+});
+
 router.post("/login", function(req, res) {
+  console.log("success-post");
   console.log(req.body);
 });
 
