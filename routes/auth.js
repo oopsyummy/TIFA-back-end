3129937da40be0ccc023ed3e6a05bcd962917541
oopsyummy <yangyangyangxiaoyu@hotmail.com>
2019-07-22 15:34:06 +0800

    login test

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,19 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  res.send("GET request to the homepage");
+  res.send("GET request to the auth");
   console.log("success");
 });
 /* POST */
+router.get("/login", function(req, res) {
+  res.send("GET request to the login");
+});
 router.post("/login", function(req, res) {
-  console.log(req.body);
+  if (req.body.username === "test" && req.body.password === "test") {
+    res.status(200, console.log(req.body)).end();
+  } else {
+    res.status(403, console.log(req.body)).end();
+  }
 });
 
 module.exports = router;
