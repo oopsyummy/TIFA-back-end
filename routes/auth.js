5537a7497060e5acca36c97518694c35a19cb89a
oopsyummy <yangyangyangxiaoyu@hotmail.com>
2019-07-25 19:42:14 +0800

    install sequelize and sqlite3

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 router.get("/", function(req, res) {
   res.send("GET request to the auth");
-  console.log("success");
+  console.log("success111");
 });
 /* POST */
 router.get("/login", function(req, res) {
   res.send("GET request to the login");
 });
 router.post("/login", function(req, res) {
-  if (req.body.username === "test" && req.body.password === "test") {
+  if (req.body.username === "Test" && req.body.password === "Test") {
     res.status(200, console.log(req.body)).end();
   } else {
     res.status(403, console.log(req.body)).end();
